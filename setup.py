7e3fcce6b59df54c40c7a89a99aaaad9a69db7bb
Georgi Valkov <georgi.t.valkov@gmail.com>
2017-09-24 10:58:32 +0200

    Bump version: 1.1.0 -> 1.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 kw = {
     'name':             'tornado-http-auth',
-    'version':          '1.1.0',
+    'version':          '1.1.1',
     'description':      'Digest and basic authentication for tornado',
     'long_description': open('README.rst').read(),
     'author':           'Georgi Valkov',
